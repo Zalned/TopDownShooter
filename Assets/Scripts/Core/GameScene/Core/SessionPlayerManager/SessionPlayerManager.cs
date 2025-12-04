913ebd7406222da5802bb68f30eb888c880156ae
zlnd <den228622@gmail.ru>
2025-12-05 00:17:02 +0500

    minor tweaks

---
+++ Assets/Scripts/Core/GameScene/Core/SessionPlayerManager/SessionPlayerManager.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SessionPlayerManager : IDisposable {
+    public Dictionary<ulong, NetworkPlayerData> SessionPlayers { get; private set; } = new();
+    public Dictionary<ulong, ActivePlayerData> ActivePlayers { get; private set; } = new();
+    public Dictionary<ulong, GameObject> LivePlayers { get; private set; } = new();
+    public Dictionary<ulong, GameObject> DeadPlayers { get; private set; } = new();
+
+    public Dictionary<ulong, ActivePlayerData> LosePlayers { get; private set; } = new();
+    public List<ulong> PlayerWhoChoseCard { get; private set; } = new();
+    public bool IsAllLosePlayersChoseCard => LosePlayers.Count == PlayerWhoChoseCard.Count;
+
+    public event Action OnLivePlayerAdded;
+    public event Action OnLivePlayerRemoved;
+
+    public void AddPlayerWhoChoseCard( ulong playerID ) {
+        if( PlayerWhoChoseCard.Contains( playerID ) ) {
+            Debug.LogWarning( $"[{nameof( SessionPlayerManager )}] Player already chose card." );
+            return;
+        }
+        PlayerWhoChoseCard.Add( playerID );
+    }
+    public void ResetPlayerWhoChoseCard() {
+        PlayerWhoChoseCard.Clear();
+    }
+    
+
+    // Session players
+    public void SetSessionPlayers( Dictionary<ulong, NetworkPlayerData> players ) {
+        foreach ( var player in players ) {
+            AddSessionPlayer( player.Key, player.Value );
+        }
+    }
+    public void ResetSessionPlayers() {
+        SessionPlayers.Clear();
+    }
+    public void AddSessionPlayer( ulong playerId, NetworkPlayerData data ) {
+        SessionPlayers.Add( playerId, data );
+    }
+    public void RemoveSessionPlayer( ulong playerId ) {
+        SessionPlayers.Remove( playerId );
+    }
+
+    // Active players
+    public void SetActivePlayers( Dictionary<ulong, NetworkPlayerData> players ) {
+        foreach ( var player in players ) {
+            AddActivePlayer( player.Key, player.Value );
+        }
+    }
+    public void ResetActivePlayers() {
+        ActivePlayers.Clear();
+    }
+    public void AddActivePlayer( ulong playerID, NetworkPlayerData data ) {
+        ActivePlayers.Add( playerID, new( data ) );
+    }
+    public void RemoveActivePlayer( ulong playerID ) {
+        ActivePlayers.Remove( playerID );
+    }
+
+    public ActivePlayerData GetActivePlayerByID( ulong id ) {
+        if ( ActivePlayers.ContainsKey( id ) ) {
+            return ActivePlayers[ id ];
+        } else { throw new Exception( "ID not fount in dictionary." ); }
+    }
+
+    public void AddWinScore( ulong id ) {
+        if ( ActivePlayers.ContainsKey( id ) ) {
+            ActivePlayers[ id ].AddScore();
+        } else { throw new Exception( "ID not fount in dictionary." ); }
+    }
+
+    // Live/Dead players
+    public void AddLivePlayer( ulong id, GameObject player ) {
+        if ( !LivePlayers.Keys.Contains( id ) ) {
+            LivePlayers.Add( id, player );
+            OnLivePlayerAdded?.Invoke();
+        } else {
+            Debug.LogWarning( $"[{nameof( SessionPlayerManager )}] Player already in LivePlayers list." );
+        }
+    }
+
+    public void RemoveLivePlayer( ulong id ) {
+        if ( LivePlayers.Remove( id ) ) {
+            OnLivePlayerRemoved?.Invoke();
+        } else {
+            Debug.LogWarning( $"[{nameof( SessionPlayerManager )}] Player not found in LivePlayers list." );
+        }
+    }
+
+    public void AddDeadPlayer( ulong id, GameObject playerObj ) {
+        if ( !DeadPlayers.Keys.Contains( id ) ) {
+            DeadPlayers.Add( id, playerObj );
+        } else {
+            Debug.LogWarning( $"[{nameof( SessionPlayerManager )}] Player already in DeadPlayers list." );
+        }
+    }
+
+    public void RemoveDeadPlayer( ulong id ) {
+        if ( DeadPlayers.Remove( id ) ) {
+
+        } else {
+            Debug.LogWarning( $"[{nameof( SessionPlayerManager )}] Player not found in DeadPlayers list." );
+        }
+    }
+
+    public ulong GetLastLifePlayerID() {
+        foreach ( var kvp in LivePlayers ) { return kvp.Key; }
+        throw new Exception( "No live players." ); // MyTodo: последний игрок может умереть после победы
+    }
+
+    // Lose players
+    public void InitializeLosePlayers( ulong winnerID ) {
+        LosePlayers.Clear();
+        foreach ( var player in ActivePlayers ) {
+            LosePlayers.Add( player.Key, player.Value );
+        }
+        LosePlayers.Remove( winnerID );
+    }
+}
---
+++ Assets/Scripts/Services/NotUsed/ReviveService/ReviveTimer.cs
@@ -0,0 +1,10 @@
+//public class ReviveTimer {
+//    private float _reviveDuration = 5f;
+//    private float _elapsedTime = 0f;
+
+//    public bool IsReviveComplete => _elapsedTime >= _reviveDuration;
+
+//    public void UpdateTime( float deltaTime ) {
+//        _elapsedTime += deltaTime;
+//    }
+//}
